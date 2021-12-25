fb65473ddff865376a3277da71a750519fd7a063
CodeBlaze <blazeingamer10@gmail.com>
2021-12-25 13:33:01 +0530

    info added

--- hello.cpp
+++ hello.cpp
@@ -2,7 +2,10 @@
 
 int main()
 {
-  std::cout << "Hello World !" << std::endl;
+  std::cout << "Hello, this is my competive programming sandbox" << std::endl;
+  std::cout << "ENV, clang-cl (installed by vs 2022) and vscode" << std::endl;
+  std::cout << "each problem is solved in a single file for portability, hence there won't be any inclues except the standard library" << std::endl;
+  std::cout << "if you are able to run/debug this simple program you all set to code" << std::endl;
 
   return 0;
 }
