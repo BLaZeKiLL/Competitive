97a0e15459cd7523c89ec67d09dba05db24e0ed1
CodeBlaze <blazeingamer10@gmail.com>
2021-12-27 13:26:40 +0530

    template added

---
+++ templates/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <string>
+
+#define LOG(x) cout << x << endl;
+#define IN(x) cin >> x;
+#define LLI long long int
+
+// PROBLEM :
+
+using namespace std;
+
+int main()
+{
+	return 0;
+}
